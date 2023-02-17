625a7e5bff9dfb53695179eb76f6f04e15f930f7
Divy <divy.jain@bombaysoftwares.com>
2023-02-17 16:52:34 +0530

    added handleAdd function to add todo & implemented type in useRef hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,26 @@ import { Todo } from './model';
  
 const App: React.FC = () => {
   const [todo,setTodo] = useState<string>("");
-  const [todos, settodos] = useState<Todo>([])
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault(); 
+
+    if (todo) {
+      setTodos([...todos,{ id:Date.now(), todo, isDone:false}]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
 
   return (
     <div className="App">
         <span className="heading">Todo App</span>
-        <InputField todo={todo} setTodo={setTodo}/>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+        {todos.map((todo) => (
+          <li>{todo.todo}</li>
+        ))}
     </div>
   );
 }
