e8aeecb54a00edcc5f48c20c60c426abb298d81a
Divy <divy.jain@bombaysoftwares.com>
2023-02-17 16:26:03 +0530

    created interface for props and todos and added onchage on todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
  
 const App: React.FC = () => {
+  const [todo,setTodo] = useState<string>("");
+  const [todos, settodos] = useState<Todo>([])
+
   return (
     <div className="App">
         <span className="heading">Todo App</span>
-        <InputField/>
+        <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import "./styles.css"
 
-const InputField = () => {
-  return <form className='input'>
-    <input type="input" placeholder="Enter a task ToDo" className='input__box'/>
-    <button className="input_submit" type="submit">
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+  return (
+    <form className='input'>
+      <input type="input" placeholder="Enter a task ToDo" className='input__box'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input_submit" type="submit">
         GO
-    </button>
-  </form>
-    
-  
+      </button>
+    </form>
+  )
 }
 
 export default InputField
